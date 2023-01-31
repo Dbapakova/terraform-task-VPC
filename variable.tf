6392daaf19bee071b234f04090906c4497c488f4
Dbapakova <116044984+Dbapakova@users.noreply.github.com>
2023-01-31 17:53:50 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,22 +6,11 @@ variable "region" {
   type    = string
 }
 
-variable "vpc_name" {
-  type        = string
-  description = "Please provide a VPC Name"
-}
-
 variable "vpc_cidr" {
   type        = string
-  description = "Please provide a VPC CIDR"
 }
 
-variable "private_subnets" {
+variable "azs" {
   type        = list(any)
-  description = "Please provide Private Subnets"
 }
 
-variable "public_subnets" {
-  type        = list(any)
-  description = "Please provide Public Subnets"
-}
